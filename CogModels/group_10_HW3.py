42e11062382cc0ff69898f6187c85d1db702bb8c
gfaline <gwenfedgar@gmail.com>
2021-10-26 17:52:35 -0400

    ?

--- CogModels/group_10_HW3.py
+++ CogModels/group_10_HW3.py
@@ -13,10 +13,10 @@ def probabilistic_bystander_model(n, temperature, altruism=.5):
 
     if n > 4:
         if altruism > 0.6:
-            if 200 > temperature > 45:
+            if 110 > temperature > 45:
                 return 1
         return 0.001
-    if altruism > .3 and 200 > temperature > 32:
+    if altruism > .3 and 110 > temperature > 32:
         return 1
     return 0.001
 
