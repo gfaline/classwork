0540ffe30d0e99f6a0e844ed59da6c45a0c62f5f
gfaline <gwenfedgar@gmail.com>
2022-03-10 16:54:51 -0500

    Can go through list, if only it navigated

--- Ethics/Agent.java
+++ Ethics/Agent.java
@@ -13,6 +13,7 @@ public class Agent extends SupermarketComponentImpl {
 
     boolean firsttime = true;
 	String state = "entry";
+	boolean hasCart = false;
 	// Options are a shelf item, counter item, cart return, checkout
 
 	String goal = "leek";
@@ -23,119 +24,115 @@ public class Agent extends SupermarketComponentImpl {
     
     @Override
     protected void executionLoop() {
-	// this is called every 100ms
-	// put your code in here
-	Observation obs = getLastObservation();
-	//System.out.println("Target at " + obs.players[0].position[0] + " " + obs.players[0].position[1]);
-	System.out.println(obs.players.length + " players at " + obs.players[0].position[0] + " " + obs.players[0].position[1]);
-	//Player player = obs.players[0];
-	// This loop will find the position of the target given that the target is among expected items
-	// Shelf usually withe 2.25 1.5 or 1.0 2.0
-	if (firsttime) {
-		System.out.println(obs.carts.length + " carts");
-		System.out.println(obs.shelves.length + " shelves");
-		System.out.println(obs.counters.length + " counters");
-		System.out.println(obs.registers.length + " registers");
-		System.out.println(obs.cartReturns.length + " cartReturns");
-		// print out the shopping list
-		System.out.println("Shoppping list: " + Arrays.toString(obs.players[0].shopping_list));
-		System.out.println(obs.players[0].shopping_list.getClass());
-		firsttime = false;
-		shopping_list = new ArrayList<String>(Arrays.asList(obs.players[0].shopping_list));
-		shopping_list.add(0, "cartReturn");
-		shopping_list.add(shopping_list.size() - 1, "register");
+		// this is called every 100ms
+		// put your code in here
+		Observation obs = getLastObservation();
+		//System.out.println("Target at " + obs.players[0].position[0] + " " + obs.players[0].position[1]);
+		System.out.println(obs.players.length + " players at " + obs.players[0].position[0] + " " + obs.players[0].position[1]);
+		//Player player = obs.players[0];
+		// This loop will find the position of the target given that the target is among expected items
+		// Shelf usually withe 2.25 1.5 or 1.0 2.0
+		if (firsttime) {
+			System.out.println(obs.carts.length + " carts");
+			System.out.println(obs.shelves.length + " shelves");
+			System.out.println(obs.counters.length + " counters");
+			System.out.println(obs.registers.length + " registers");
+			System.out.println(obs.cartReturns.length + " cartReturns");
+			// print out the shopping list
+			System.out.println("Shoppping list: " + Arrays.toString(obs.players[0].shopping_list));
+			System.out.println(obs.players[0].shopping_list.getClass());
+			firsttime = false;
+			shopping_list = new ArrayList<String>(Arrays.asList(obs.players[0].shopping_list));
+			shopping_list.add(0, "cartReturn");
+			shopping_list.add(shopping_list.size(), "register");
 
-		//goal = shopping_list.get(1);
+			goal = shopping_list.get(0);
+			System.out.println(goal);
+			System.out.println("New Shoppping list: " + shopping_list);
+
+		}
 		System.out.println(goal);
 
-	}
-	System.out.println(goal);
-
-	if (goal.equals("checkout") || goal.equals("register")){
-		relevantObj = obs.registers[0];
-		goalPosition = relevantObj.position;
-		//System.out.println("Shelf height and width: " + relevantObj.height + " " + relevantObj.width);
-	}
-
-	if (goal.equals("cartReturn")){
-		relevantObj = obs.cartReturns[0];
-		goalPosition = relevantObj.position;
-		//System.out.println("Shelf height and width: " + relevantObj.height + " " + relevantObj.width);
-	}
-
-	for (Observation.Counter ct : obs.counters){
-		if (ct.food.equals(goal)){
-			relevantObj = ct;
-			goalPosition = ct.position;
-			//System.out.println("Shelf height and width: " + ct.height + " " + ct.width);
+		if (goal.equals("checkout") || goal.equals("register")) {
+			relevantObj = obs.registers[0];
+			goalPosition = relevantObj.position;
+			//System.out.println("Shelf height and width: " + relevantObj.height + " " + relevantObj.width);
 		}
-	}
 
-	for(Observation.Shelf sh: obs.shelves){
-		if (sh.food.equals(goal)){
-			relevantObj = sh;
-			goalPosition = sh.position;
-			//System.out.println("Shelf height and width: " + sh.height + " " + sh.width);
+		if (goal.equals("cartReturn")) {
+			relevantObj = obs.cartReturns[0];
+			goalPosition = relevantObj.position;
+			//System.out.println("Shelf height and width: " + relevantObj.height + " " + relevantObj.width);
 		}
-	}
 
-	if (relevantObj==null){
-		System.out.println("There is no target object, it will just error from here");
-	}
-	System.out.println("Target at " + goalPosition[0] + " " + goalPosition[1]);
+		for (Observation.Counter ct : obs.counters) {
+			if (ct.food.equals(goal)) {
+				relevantObj = ct;
+				goalPosition = ct.position;
+				//System.out.println("Shelf height and width: " + ct.height + " " + ct.width);
+			}
+		}
 
-	//System.out.println(obs.interactive_stage);
-	//System.out.println(obs.players[0].position[1]);
+		for (Observation.Shelf sh : obs.shelves) {
+			if (sh.food.equals(goal)) {
+				relevantObj = sh;
+				goalPosition = sh.position;
+				//System.out.println("Shelf height and width: " + sh.height + " " + sh.width);
+			}
+		}
 
-	// if the height is within 1 of the position, it can just go sideways... Unless it's a counter or checkout.
-	//
-	double nextX = obs.players[0].position[0];
-	double nextY = obs.players[0].position[1];
+		if (relevantObj == null) {
+			System.out.println("There is no target object, it will just error from here");
+		}
+		System.out.println("Target at " + goalPosition[0] + " " + goalPosition[1]);
 
-	if (goal.equals("checkout") || goal.equals("register") || goal.equals("cartReturn")){
-		if (!atHub)
-			goToAisleHub(obs, obs.players[0]);
-		if (goal.equals("checkout") || goal.equals("register")){
+		//System.out.println(obs.interactive_stage);
+		//System.out.println(obs.players[0].position[1]);
+
+		// if the height is within 1 of the position, it can just go sideways... Unless it's a counter or checkout.
+		//
+		double nextX = obs.players[0].position[0];
+		double nextY = obs.players[0].position[1];
+
+		if (goal.equals("checkout") || goal.equals("register") || goal.equals("cartReturn")) {
+			if (!atHub)
+				goToAisleHub(obs, obs.players[0]);
+			if (goal.equals("checkout") || goal.equals("register")) {
+				goToY(obs, obs.players[0], goalPosition[1]);
+				approachRegister(obs, relevantObj, obs.players[0]);
+			} else {
+				System.out.println(relevantObj.position[1]);
+				approachCartReturn(obs, relevantObj, obs.players[0]);
+			}
+		} else if (relevantObj.getClass().equals(Observation.Counter.class)) {
+			System.out.println("The class equals gets a result of a counter item");
+			if (!atHub)
+				goToRearAisleHub(obs, obs.players[0]);
 			goToY(obs, obs.players[0], goalPosition[1]);
-			approachRegister(obs, relevantObj, obs.players[0]);
+			approachCounter(obs, relevantObj, obs.players[0]);
+			//goToX(obs, obs.players[0], goalPosition[0]);
+			//Here we want to be inRearAisleHub or besideCounters
+		} else if (relevantObj.getClass().equals(Observation.Shelf.class)) {
+			boolean inAisle = amInAisle(obs, obs.players[0]);
+			//goTo aisle hub. Go to y cord - width. Go to x cord. go north/south (whichever is the one supposed to)
+			if (!atHub)
+				goToAisleHub(obs, obs.players[0]);
+			// Has it go south of the aisle
+			goToY(obs, obs.players[0], relevantObj.position[1] + (Math.ceil(relevantObj.height / 2)) + .1);
+			goToX(obs, obs.players[0], relevantObj.position[0] + .7, relevantObj.position[1] + (Math.ceil(relevantObj.height / 2)) + .1);
+			approachShelf(obs, relevantObj, obs.players[0]);
+			//goNorth();
+
 		}
-		else{
-			System.out.println(relevantObj.position[1]);
-			approachCartReturn(obs, relevantObj, obs.players[0]);
-		}
-	}
-
-	else if (relevantObj.getClass().equals(Observation.Counter.class)){
-		System.out.println("The class equals gets a result of a counter item");
-		if (!atHub)
-			goToRearAisleHub(obs, obs.players[0]);
-		goToY(obs, obs.players[0], goalPosition[1]);
-		approachCounter(obs, relevantObj, obs.players[0]);
-		//goToX(obs, obs.players[0], goalPosition[0]);
-		//Here we want to be inRearAisleHub or besideCounters
-	}
-
-	else if (relevantObj.getClass().equals(Observation.Shelf.class)){
-		boolean inAisle = amInAisle(obs, obs.players[0]);
-		//goTo aisle hub. Go to y cord - width. Go to x cord. go north/south (whichever is the one supposed to)
-		if (!atHub)
-			goToAisleHub(obs, obs.players[0]);
-		// Has it go south of the aisle
-		goToY(obs, obs.players[0], relevantObj.position[1] + (Math.ceil(relevantObj.height/2)) + .1);
-		goToX(obs, obs.players[0], relevantObj.position[0] + .7, relevantObj.position[1] + (Math.ceil(relevantObj.height/2)) + .1);
-		approachShelf (obs, relevantObj, obs.players[0]);
-		//goNorth();
 
 	}
-
-    }
 
 	private void goToAisleHub(Observation obs, Observation.Player ply){
 		double x = ply.position[0];
 		double y = ply.position[1];
 
-		if (!obs.inAisleHub(ply.index)){
-			//System.out.println("Player x y: " + x + " " + y);
+		if (!obs.inAisleHub(ply.index) && !acceptableYNearness(ply)){
+			System.out.println("I am going to the aisle hub");
 			if (x > (4.5 - ply.width)){
 				//Check for collission on next step
 				double next = x + .3;
@@ -195,7 +192,8 @@ public class Agent extends SupermarketComponentImpl {
 		double x = ply.position[0];
 		double y = ply.position[1];
 
-		if (!obs.inRearAisleHub(ply.index)){
+		if (!obs.inRearAisleHub(ply.index) && !acceptableYNearness(ply)){
+			System.out.println("I am going to the rear aisle hub");
 			//System.out.println("Player x y: " + x + " " + y);
 			if (x < (15.5 + ply.width)){
 				//Check for collission on next step
@@ -283,23 +281,25 @@ public class Agent extends SupermarketComponentImpl {
 		double vol_ydiff = (targetY + (relevantObj.height/2.0)) - ply.position[1];
 		double width_diff =  (relevantObj.height/2.0) + ply.width/2.0;
 		//if(ydiff > .3 || (ply.position[1] + ply.width/2.0  > targetY + relevantObj.height/2.0 && ply.direction == 1)){
-		if(ydiff > .4 || (vol_ydiff > .06 && vol_ydiff <= .2 && vol_ydiff > 0 && ply.direction == 1)){
-			System.out.println("I need to move in the Y direction");
-			System.out.println("The volitile difference is " + vol_ydiff);
-			if (targetY + (relevantObj.height/2.0) > ply.position[1]){
-				if (ply.direction != 1)
+		if (obs.inAisleHub(ply.index) || obs.inRearAisleHub(ply.index)) {
+			if (ydiff > .4 || (vol_ydiff > .06 && vol_ydiff <= .2 && vol_ydiff > 0 && ply.direction == 1)) {
+				System.out.println("I need to move in the Y direction");
+				System.out.println("The volitile difference is " + vol_ydiff);
+				if (targetY + (relevantObj.height / 2.0) > ply.position[1]) {
+					if (ply.direction != 1)
+						goSouth();
 					goSouth();
-				goSouth();
-			}
-			else if (ydiff > .4){
-				if (ply.direction != 0)
+				} else if (ydiff > .4) {
+					if (ply.direction != 0)
+						goNorth();
 					goNorth();
-				goNorth();
+				}
 			}
 		}
 	}
 
 	private void goToX(Observation obs, Observation.Player ply, double targetX, double targetY){
+		//double xdiff = Math.abs(targetX +  (relevantObj.width/2.0) - ply.position[0]);
 		double xdiff = Math.abs(targetX - ply.position[0]);
 		double ydiff = Math.abs(targetY + (relevantObj.height/2.0) - ply.position[1]);
 		if (ydiff < .5 && xdiff > .3){
@@ -371,8 +371,14 @@ public class Agent extends SupermarketComponentImpl {
 			goSouth();
 		}
 
-		if(obs.atCartReturn(ply.index)){
-			toggleShoppingCart();
+		if(obs.atCartReturn(ply.index) && !hasCart){
+			System.out.println("I am at the cart return");
+			interactWithObject();
+			hasCart = true;
+			goNorth();
+			goNorth();
+			goNorth();
+			crossOffItem();
 		}
 	}
 
@@ -403,6 +409,33 @@ public class Agent extends SupermarketComponentImpl {
 
 	private boolean amAtBottom(Observation obs, Observation.Player ply){
 		return false;
+	}
+
+	private boolean acceptableYNearness(Observation.Player ply){
+		double targetY = goalPosition[1];
+		double ydiff = Math.abs(targetY +  (relevantObj.height/2.0) - ply.position[1]);
+		double vol_ydiff = (targetY + (relevantObj.height/2.0)) - ply.position[1];
+		double width_diff =  (relevantObj.height/2.0) + ply.width/2.0;
+
+		return (ydiff > .4 || (vol_ydiff > .06 && vol_ydiff <= .2 && vol_ydiff > 0 && ply.direction == 1));
+
+	}
+
+	private void crossOffItem(){
+		if (shopping_list.size() > 0) {
+			shopping_list.remove(0);
+		}
+		if (shopping_list.size() > 0) {
+			goal = shopping_list.get(0);
+		}
+		else{
+			leave();
+		}
+
+	}
+
+	private boolean leave(){
+		return true;
 	}
 
 	//I am not sure if 0 is the top or bottom aisle
