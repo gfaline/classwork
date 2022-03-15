4cbe9da701183e21d79ebdd0cbd1671a5e562de8
gfaline <gwenfedgar@gmail.com>
2022-03-15 16:01:01 -0400

    Same aisle

--- Ethics/Agent.java
+++ Ethics/Agent.java
@@ -51,10 +51,11 @@ public class Agent extends SupermarketComponentImpl {
 			System.out.println("Shoppping list: " + Arrays.toString(player.shopping_list));
 			System.out.println(player.shopping_list.getClass());
 			firsttime = false;
-			//shopping_list = new ArrayList<String>(Arrays.asList(player.shopping_list));
+			shopping_list = new ArrayList<String>(Arrays.asList(player.shopping_list));
 			shopping_list = new ArrayList<String>();
-			shopping_list.add(0, "fish");
-			shopping_list.add(0, "milk");
+			//shopping_list.add(0, "raspberry");
+			shopping_list.add(0, "banana");
+			shopping_list.add(0, "raspberry");
 
 			// shopping_list.add(0, "fish");
 			// shopping_list.add(0,"prepared foods");
@@ -68,7 +69,7 @@ public class Agent extends SupermarketComponentImpl {
 			shopping_list.add(0, "cartReturn");
 			shopping_list.add(shopping_list.size(), "register");
 
-			//quantity_list = new ArrayList<Integer>(Arrays.stream(player.list_quant).boxed().toList());
+			quantity_list = new ArrayList<Integer>(Arrays.stream(player.list_quant).boxed().toList());
 			quantity_list = new ArrayList<Integer>();
 			quantity_list.add(0, 1);
 			quantity_list.add(0, 1);
@@ -192,7 +193,8 @@ public class Agent extends SupermarketComponentImpl {
 				// you don't have enough of the thing that you need
 				if (!atHub)
 					goToAisleHub(obs, player);
-				approachShelf(obs, relevantObj, player);
+				else
+					approachShelf(obs, relevantObj, player);
 				if (playerIsHoldingFood(player) /*&& !playerIsHoldingCart(player)*/) {
 					System.out.println("I would be returning to the cart here");
 					// returnToCart(obs, player);
@@ -682,7 +684,7 @@ public class Agent extends SupermarketComponentImpl {
 			// player is holding cart. need to navigate to the shelf
 			if (playerIsFacingWest(ply)) {
 				// park the cart on the left side of the shelf 
-				x_target = relevantObj.position[0];
+				x_target = relevantObj.position[0] - .35;
 
 			} else if (playerIsFacingEast(ply)) {
 				// park the cart on the right side of the shelf 
